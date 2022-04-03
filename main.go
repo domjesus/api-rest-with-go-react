aa1e9f551f3777a3306b7e23bdb871e2fed2cdac
Wandeir Carneiro de Souza <domjesus@gmail.com>
2022-04-03 14:08:53 -0300

    First commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"api_go_rest/database"
+	"api_go_rest/routes"
+	"fmt"
+)
+
+func main() {
+
+	database.ConectaComBancoDeDados()
+
+	// models.Personalidades = []models.Personalidade{
+	// 	{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+	// 	{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	// }
+
+	fmt.Println("Iniciando o servidor")
+
+	routes.HandleRequest()
+}
